cc5af389563708d434fe33954009fd81786bbae6
YLCHUN <ylchun@ylchundemacbook-pro.local>
2016-04-06 21:33:33 +0800

    Initial Commit

---
+++ WWOCTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWOCTextDemo
+//
+//  Created by YLCHUN on 16/4/6.
+//  Copyright © 2016年 YLCHUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWOCTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWOCTextDemo
+//
+//  Created by YLCHUN on 16/4/6.
+//  Copyright © 2016年 YLCHUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
